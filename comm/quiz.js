e56018085198bd02884e8953e8320ef65e47bb25
Hillman7 <moriancumeracma@hotmail.com>
2017-06-07 16:26:10 -0600

    Made a comm site

---
+++ comm/quiz.js
@@ -0,0 +1,34 @@
+function avoid()
+{
+
+    var display = "No! That's not what I want aren't you listening?!";
+    document.getElementById("output").innerHTML = display;
+}
+
+
+
+
+function defense()
+{
+    var display = "Errie silence... I know that you have, I just feel that I still don't have the time that I need with you."
+
+
+    document.getElementById("output").innerHTML = display;
+
+}
+
+function attack()
+{
+
+    var display = "Your wife leaves the room."
+    document.getElementById("output").innerHTML = display;
+
+}
+
+function solution()
+{
+    var display = "I don't know, can you help me think of a solution?"
+    document.getElementById("output").innerHTML = display;
+
+}
+
