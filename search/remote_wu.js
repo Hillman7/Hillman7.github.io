485d3470e7f0a0600f1fc2bf962d1fac81cad126
Hillman7 <moriancumeracma@hotmail.com>
2017-06-29 17:44:58 -0600

    added two activities

---
+++ search/remote_wu.js
@@ -0,0 +1,56 @@
+// Current Location Scripts
+$(function () {
+
+    var status = $('#status');
+
+    (function getGeoLocation() {
+        status.text('Getting Location...');
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var lat = position.coords.latitude;
+                var long = position.coords.longitude;
+
+                // Call the getData function, send the lat and long
+                getData(lat, long);
+
+            });
+        } else {
+            status.text("Your browser doesn't support Geolocation or it is not enabled!");
+        }
+
+    })();
+
+    // Get the data from the wunderground API
+
+    function getData(lat, long){
+        $.ajax({
+            url:"https://api.wunderground.com/api/b312dac5c9192176/geolookup/conditions/q/" + lat + "," + long + ".json",
+            dataType : "jsonp",
+            success : function(parsed_json) {
+                var location = parsed_json['location']['city'];
+                var temp_f = parsed_json['current_observation']['temp_f'];
+                var state = parsed_json['location']['state'];
+                var summary = parsed_json['current_observation']  ['weather'];
+                var wind = parsed_json['current_observation']['wind_mph'];
+                var precep = parsed_json['current_observation']['precip_today_in'];
+                var feel = parsed_json['current_observation']['feelslike_f'];
+                var update = parsed_json['current_observation']['observation_time'];
+                $('#cityDisplay').html(location + ',' + ' ' + state);
+                $('#currentTemp').html(Math.round(temp_f) + "&#176");
+                $('#summary').html(summary);
+                $('#add1').html("Wind in MPH:" + ' ' + wind);
+                $('#add2').html("The Percepitation today is " + precep);
+                $('#add3').html('Today feels like:' + ' ' + Math.round(feel) + '&#176');
+                $('#update').html(update);
+                $("#cover").fadeOut(250);
+
+            }
+        });
+
+    }
+
+    // A function for changing a string to TitleCase
+    function toTitleCase(str){
+        return str.replace(/\w+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    }
+});
