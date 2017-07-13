33191e321cb2548133ea3ee0fa02b3cb70bb536d
Hillman7 <moriancumeracma@hotmail.com>
2017-07-13 15:54:31 -0600

    added anvil content

--- acme/js/content_final.js
+++ acme/js/content_final.js
@@ -1,68 +1,137 @@
+
+
+
+
+
+
+var content = "";
+
+
+
+// Intercept the menu link clicks
+$("#navigation").on("click", "a", function (evt) {
+    evt.preventDefault();
+    // With the text value get the needed value from the weather.json file
+    var jsonCity = $(this).text(); // Franklin, etc...
+//    console.log(jsonCity);
+    $.ajax({
+        url: "/Hillman7.github.io/acme/js/acme.json"
+        , dataType: "json"
+        , success: function (content) {
+
+
+            // hide the home page content.
+            $('#home_content').css("display","none");
+
+
+
+            // show the content of stuff to be shown.
+            $('#product_content').css("visibility","visible");
+
+
+
+
+
+
+
+/*******************************************************************************/
+
+            // grab the Name info from json file.
+            var anvilName = content['Anvils']['name'];
+            console.log(anvilName);
+
+            // this is the product name to go into the h2 tag.
+            document.getElementById('product_name').innerHTML = anvilName;
+
 //
-//
-//
-//
-//
-//
-//var content = "";
-//
-//
-//
-//// Intercept the menu link clicks
-//$("#navigation").on("click", "a", function (evt) {
-//    evt.preventDefault();
-//    // With the text value get the needed value from the weather.json file
-//    var jsonCity = $(this).text(); // Franklin, etc...
-////    console.log(jsonCity);
-//    $.ajax({
-//        url: "/Hillman7.github.io/acme/js/acme.json"
-//        , dataType: "json"
-//        , success: function (content) {
-//           // console.log(content);
-//            console.log(content['Anvils']['name']);
-//
-//
-//            // grab the home info from json file.
-//            var anvil = content['Anvils']['name'];
-//            console.log(anvil);
-//
-//
-//            // grab the explosive info
-//
-//
-//
-//
-//
-//            // grab the decoys
-//
-//
-//
-//
-//
-//            // grab the traps.
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//// put it in.
-//document.getElementById('').innerHTML = ;
-//
-//
-//
-//
-//
-//        }
-//    });
-//});
+
+            // picture goes here.
+            $('#insertimage').prepend('<img id="product_picture" src="/Hillman7.github.io/acme/images/products/anvil.png" />')
+
+
+
+
+
+
+            // this is the descriptive paragraph.
+            var anvilParagraph = content['Anvils']['description'];
+            // this goes into the paragraph tag.
+            document.getElementById('product_description').innerHTML = anvilParagraph;
+
+
+
+
+            // this is the made by paragraph
+            var manufacturer = content['Anvils']['manufacturer'];
+            document.getElementById('product_manufacturer').innerHTML += manufacturer;
+
+
+
+
+
+
+            // this is the review paragraph
+            var review = content['Anvils']['reviews'];
+            document.getElementById('product_reviews').innerHTML += review;
+            document.getElementById('product_reviews').innerHTML += "/5 stars";
+
+
+
+
+
+
+            // this is the price h1 tag.
+            var price = content['Anvils']['price'];
+            document.getElementById('product_price').innerHTML += price;
+
+
+
+/*******************************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+            // grab the explosive info
+            var explosive = content['Explosives']['name'];
+
+
+
+
+            // grab the decoys
+
+
+
+
+
+            // grab the traps.
+
+
+
+
+
+
+
+
+
+
+
+        }
+    });
+});
+
+
+
+
+
+
+
 
 
 
